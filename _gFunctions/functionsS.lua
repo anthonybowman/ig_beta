77d69f766a142ff3ab3a8ffce5cbb786672b352b
Czakó Csaba <czakocsaba02@gmail.com>
2017-04-06 15:33:42 +0200

    _gFunctions script

--- _gFunctions/functionsS.lua
+++ _gFunctions/functionsS.lua
@@ -15,3 +15,41 @@ function startMod(resource)
 	setRuleValue("Mod Version", tostring(defaultSettings["modVersion"]))
 end
 addEventHandler("onResourceStart", getResourceRootElement(getThisResource()), startMod)
+
+
+local characterID = {}
+addEventHandler("onPlayerJoin",getRootElement(),
+	function()
+		local slot = nil
+		for i = 1, 1024 do
+			if (characterID[i]==nil) then
+				slot = i
+				break
+			end
+		end
+		characterID[slot] = source
+		setElementData(source, "_character:id", slot)
+	end
+)
+addEventHandler("onPlayerQuit",getRootElement(),
+	function()
+		local slot = getElementData(source, "_character->id")
+		if (slot) then
+			characterID[slot] = nil
+		end
+	end
+)
+addEventHandler("onResourceStart",getResourceRootElement(getThisResource()),
+	function()
+		local players = getElementsByType("player")
+		for key, value in ipairs(players) do
+			characterID[key] = value
+			setElementData(value, "_character->id", key)
+		end
+	end
+)
+
+function onPlayerNameChange()
+	cancelEvent()
+end
+addEventHandler("onPlayerChangeNick", getRootElement(), onPlayerNameChange)
