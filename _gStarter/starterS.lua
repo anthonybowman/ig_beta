b0ae0b5efed1b72d1de46617745ce219e1f2a198
Czakó Csaba <czakocsaba02@gmail.com>
2017-04-06 14:17:43 +0200

    _gStarter script

--- _gStarter/starterS.lua
+++ _gStarter/starterS.lua
@@ -11,6 +11,7 @@
 function startScripts()
 	local resources = getResources()
 	startResource("_gFunctions")
+	startResource("_gCompiler")
 	startResource("_gWhitelist")
 	startResource("_gDatabase")
 	for k,v in ipairs(resources) do
