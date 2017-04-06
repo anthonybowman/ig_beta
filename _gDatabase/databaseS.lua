33f8121f370c7aa9f82a04df416bca90788124cc
Czakó Csaba <czakocsaba02@gmail.com>
2017-04-06 14:55:15 +0200

    _gDatabase script

---
+++ _gDatabase/databaseS.lua
@@ -0,0 +1,41 @@
+--[[
+*******************************************************
+	Project owner:		Krumpli2, Thug
+	Project name: 		internalGaming
+	Developers:   		Czakó Csaba
+	Version:     		v1.0 Open Source
+	Status:      		Stable release
+********************************************************
+--]]
+
+local dbHost = "127.0.0.1"
+local dbUser = ""
+local dbPass = ""
+local dbName = ""
+
+local sqlConnection = nil
+
+function enabledConnection(res)
+	sqlHandler = dbConnect( "mysql", "dbname="..dbName..";host="..dbHost, dbUser, dbPass, "charset=utf8" )
+	
+	if (not sqlHandler) then
+		if (res == getThisResource()) then
+	--		outputDebugString("Nem sikerült csatlakozni az adatbázishoz!")
+		end
+		return nil
+	else
+		dbExec( sqlHandler,"SET NAMES utf8" )
+	--	outputDebugString("Sikerült csatlakozni az adatbázishoz!")
+	end
+	
+	return nil
+end
+addEventHandler("onResourceStart", getResourceRootElement(getThisResource()), enabledConnection, false)
+	
+function getConnection()
+	if(sqlHandler) then
+		return sqlHandler
+	else
+		return nil
+	end
+end
