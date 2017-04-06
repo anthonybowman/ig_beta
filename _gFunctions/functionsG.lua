196e8825c5e6ce4c588b08d80e10c6caf54b83e8
Czakó Csaba <czakocsaba02@gmail.com>
2017-04-06 16:00:20 +0200

    _gFunctions script

--- _gFunctions/functionsG.lua
+++ _gFunctions/functionsG.lua
@@ -33,3 +33,82 @@ defaultSettings = {
 function getDefaultSettings()
 	return defaultSettings
 end
+
+function Check(funcname, ...)
+    local arg = {...}
+ 
+    if (type(funcname) ~= "string") then
+        error("Argument type mismatch at 'Check' ('funcname'). Expected 'string', got '"..type(funcname).."'.", 2)
+    end
+    if (#arg % 3 > 0) then
+        error("Argument number mismatch at 'Check'. Expected #arg % 3 to be 0, but it is "..(#arg % 3)..".", 2)
+    end
+ 
+    for i=1, #arg-2, 3 do
+        if (type(arg[i]) ~= "string" and type(arg[i]) ~= "table") then
+            error("Argument type mismatch at 'Check' (arg #"..i.."). Expected 'string' or 'table', got '"..type(arg[i]).."'.", 2)
+        elseif (type(arg[i+2]) ~= "string") then
+            error("Argument type mismatch at 'Check' (arg #"..(i+2).."). Expected 'string', got '"..type(arg[i+2]).."'.", 2)
+        end
+ 
+        if (type(arg[i]) == "table") then
+            local aType = type(arg[i+1])
+            for _, pType in next, arg[i] do
+                if (aType == pType) then
+                    aType = nil
+                    break
+                end
+            end
+            if (aType) then
+                error("Argument type mismatch at '"..funcname.."' ('"..arg[i+2].."'). Expected '"..table.concat(arg[i], "' or '").."', got '"..aType.."'.", 3)
+            end
+        elseif (type(arg[i+1]) ~= arg[i]) then
+            error("Argument type mismatch at '"..funcname.."' ('"..arg[i+2].."'). Expected '"..arg[i].."', got '"..type(arg[i+1]).."'.", 3)
+        end
+    end
+end
+
+local gWeekDays = { "vasárnap", "hétfő", "kedd", "szerda", "csütörtök", "péntek", "szombat" }
+function formatDate(format, escaper, timestamp)
+	Check("formatDate", "string", format, "format", {"nil","string"}, escaper, "escaper", {"nil","string"}, timestamp, "timestamp")
+ 
+	escaper = (escaper or "'"):sub(1, 1)
+	local time = getRealTime(timestamp)
+	local formattedDate = ""
+	local escaped = false
+ 
+	time.year = time.year + 1900
+	time.month = time.month + 1
+ 
+	local datetime = { d = ("%02d"):format(time.monthday), h = ("%02d"):format(time.hour), i = ("%02d"):format(time.minute), m = ("%02d"):format(time.month), s = ("%02d"):format(time.second), w = gWeekDays[time.weekday+1]:sub(1, 2), W = gWeekDays[time.weekday+1], y = tostring(time.year):sub(-2), Y = time.year }
+ 
+	for char in format:gmatch(".") do
+		if (char == escaper) then escaped = not escaped
+		else formattedDate = formattedDate..(not escaped and datetime[char] or char) end
+	end
+ 
+	return formattedDate
+end
+
+function isLeapYear(year)
+    if year then year = math.floor(year)
+    else year = getRealTime().year + 1900 end
+    return ((year % 4 == 0 and year % 100 ~= 0) or year % 400 == 0)
+end
+ 
+function getTimestamp(year, month, day, hour, minute, second)
+    local monthseconds = { 2678400, 2419200, 2678400, 2592000, 2678400, 2592000, 2678400, 2678400, 2592000, 2678400, 2592000, 2678400 }
+    local timestamp = 0
+    local datetime = getRealTime()
+    year, month, day = year or datetime.year + 1900, month or datetime.month + 1, day or datetime.monthday
+    hour, minute, second = hour or datetime.hour, minute or datetime.minute, second or datetime.second
+
+    for i=1970, year-1 do timestamp = timestamp + (isLeapYear(i) and 31622400 or 31536000) end
+    for i=1, month-1 do timestamp = timestamp + ((isLeapYear(year) and i == 2) and 2505600 or monthseconds[i]) end
+    timestamp = timestamp + 86400 * (day - 1) + 3600 * hour + 60 * minute + second
+ 
+    timestamp = timestamp - 3600
+    if datetime.isdst then timestamp = timestamp + 3600 end
+ 
+    return timestamp
+end
